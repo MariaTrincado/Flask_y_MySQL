d18fbfd3e115b018886ddadef0a5c83358b1d3a4
María Trincado <maria_trincado@hotmail.com>
2023-07-05 23:39:30 -0400

    27Jul_UsuarioCR

--- usuarios CR/aplicación/app_usuarios/controladores/controlador_usuarios.py
+++ usuarios CR/aplicación/app_usuarios/controladores/controlador_usuarios.py
@@ -1,17 +1,6 @@
-from flask import render_template, session, request, redirect
+from flask import render_template, request, redirect
 from app_usuarios import app
 from app_usuarios.modelos.modelo_usuarios import Usuarios
-
-usuarios =[{
-        "nombre" : "Peter",
-        "apellido" : "Valdivia",
-        "correo": "ps@gmail.com"
-    },
-        {
-        "nombre" : "maria",
-        "apellido" : "alvarez",
-        "correo": "mc@gmail.com"
-    }]
 
 @app.route("/usuario", methods = ['GET'])
 def obtener_usuario():
@@ -20,10 +9,6 @@ def obtener_usuario():
 
 @app.route("/form/usuario", methods = ['GET'])
 def desplegar_formulario_usuario():
-    if 'nombre' in session:
-        print (session['nombre'])
-    else:
-        session['nombre'] = 'Alexander'
     return render_template("crear.html")
 
 @app.route("/nuevo/usuario", methods = ['POST'])
